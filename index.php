039f8f2bddc101d99310c6dc9dc054659d6d86fd
AlexxBelous <connectionwithme1@gmail.com>
2023-10-08 12:38:19 +0300

    from 19 min

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
 
-echo "Hello New Project!";
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Application();
+
+$app->router->get('/', function (){
+    return "Hello world!!!";
+});
+
+$app->run();
